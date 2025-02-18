bb84e2c5ecd63eb453ccaeaf89855bf702c070c0
Cola1000 <163750851+RhioBimoSTEI@users.noreply.github.com>
2025-02-18 08:17:57 +0700

    Import packages into Main.java

--- src/com/iqpuzzlersolver/Main.java
+++ src/com/iqpuzzlersolver/Main.java
@@ -1,5 +1,22 @@
 package com.iqpuzzlersolver;
 
+import java.io.*;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        String inputFile = "test/test.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+            // Line 1: N M P
+                     
+            // Line 2: Puzzle type (S)
+
+            // Line 3++: Puzzle Pieces
+
+            // Solver
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
